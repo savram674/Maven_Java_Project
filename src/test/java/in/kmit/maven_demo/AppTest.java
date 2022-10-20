4bdc9c2e34b2b8ac2cb84b7ce73e5f7f6ace8a8c
savram674 <savitharamesh674@gmail.com>
2022-10-20 10:29:00 +0530

    maven java project first commit

---
+++ src/test/java/in/kmit/maven_demo/AppTest.java
@@ -0,0 +1,19 @@
+package in.kmit.maven_demo;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    
+	@Test
+    public void appTest()
+    {
+    	System.out.println("\n.......Hello World Test ...!\n");
+    }
+}
